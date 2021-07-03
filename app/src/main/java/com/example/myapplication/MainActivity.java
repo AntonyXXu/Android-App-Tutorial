dae0a61313a9ebadc11bc43370ca308332651d53
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-07-03 09:53:27 -0600

    build new activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
     public void handleTextView(View v) {
         EditText text = findViewById(R.id.nameTextField);
         String input = text.getText().toString();
+        Button b = (Button) v;
+        Toast.makeText(this, input, Toast.LENGTH_LONG).show();
+
         Log.d("info", input);
+    }
+
+    public void launchSettings(View v) {
+        // launch a new activity
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
+
     }
 }
